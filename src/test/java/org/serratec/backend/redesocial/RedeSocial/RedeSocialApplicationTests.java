54c7f5016e2faf554bf91e57954e953016bedcea
Amanda Bernardes <amanda.38005@hotmail.com>
2024-05-21 23:10:34 -0300

    Subindo base do projeto

---
+++ src/test/java/org/serratec/backend/redesocial/RedeSocial/RedeSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package org.serratec.backend.redesocial.RedeSocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedeSocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
