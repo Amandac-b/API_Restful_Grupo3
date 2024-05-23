7be131bd48a1a51fc447a1fae69ab91d99ca39bb
EDUMKS <eduardokappaun2005@gmail.com>
2024-05-22 21:02:07 -0300

    AddConfg

---
+++ src/main/java/org/serratec/backend/redesocial/config/ConfigSeguranca.java
@@ -0,0 +1,65 @@
+package org.serratec.backend.redesocial.config;
+
+public class ConfigSeguranca {
+	private int minTamanhoSenha;
+	private int maxTentativasLogin;
+	private boolean autenticacaoDoisFatores;
+	private boolean controleDeConteudo;
+	private boolean notificacaoDeAtividadeSuspeita;
+
+	public ConfigSeguranca(int minTamanhoSenha, int maxTentativasLogin, boolean autenticacaoDoisFatores,
+			boolean controleDeConteudo, boolean notificacaoDeAtividadeSuspeita) {
+		this.minTamanhoSenha = minTamanhoSenha;
+		this.maxTentativasLogin = maxTentativasLogin;
+		this.autenticacaoDoisFatores = autenticacaoDoisFatores;
+		this.controleDeConteudo = controleDeConteudo;
+		this.notificacaoDeAtividadeSuspeita = notificacaoDeAtividadeSuspeita;
+	}
+
+	public int getMinTamanhoSenha() {
+		return minTamanhoSenha;
+	}
+
+	public void setMinTamanhoSenha(int minTamanhoSenha) {
+		this.minTamanhoSenha = minTamanhoSenha;
+	}
+
+	public int getMaxTentativasLogin() {
+		return maxTentativasLogin;
+	}
+
+	public void setMaxTentativasLogin(int maxTentativasLogin) {
+		this.maxTentativasLogin = maxTentativasLogin;
+	}
+
+	public boolean isAutenticacaoDoisFatores() {
+		return autenticacaoDoisFatores;
+	}
+
+	public void setAutenticacaoDoisFatores(boolean autenticacaoDoisFatores) {
+		this.autenticacaoDoisFatores = autenticacaoDoisFatores;
+	}
+
+	public boolean isControleDeConteudo() {
+		return controleDeConteudo;
+	}
+
+	public void setControleDeConteudo(boolean controleDeConteudo) {
+		this.controleDeConteudo = controleDeConteudo;
+	}
+
+	public boolean isNotificacaoDeAtividadeSuspeita() {
+		return notificacaoDeAtividadeSuspeita;
+	}
+
+	public void setNotificacaoDeAtividadeSuspeita(boolean notificacaoDeAtividadeSuspeita) {
+		this.notificacaoDeAtividadeSuspeita = notificacaoDeAtividadeSuspeita;
+	}
+
+	@Override
+	public String toString() {
+		return "ConfigSeguranca{" + "minTamanhoSenha=" + minTamanhoSenha + ", maxTentativasLogin=" + maxTentativasLogin
+				+ ", autenticacaoDoisFatores=" + autenticacaoDoisFatores + ", controleDeConteudo=" + controleDeConteudo
+				+ ", notificacaoDeAtividadeSuspeita=" + notificacaoDeAtividadeSuspeita + '}';
+	}
+}
