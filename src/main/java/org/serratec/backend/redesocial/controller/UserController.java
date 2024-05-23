056806c78a8712877d1a0938f322de66af7e2031
filipelomba <filipelomba@yahoo.com.br>
2024-05-23 18:47:22 -0300

    feat: controller , repository e service do USER

--- src/main/java/org/serratec/backend/redesocial/controller/UserController.java
+++ src/main/java/org/serratec/backend/redesocial/controller/UserController.java
@@ -1,10 +1,12 @@
 package org.serratec.backend.redesocial.controller;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
 import org.serratec.backend.redesocial.model.User;
 import org.serratec.backend.redesocial.repository.UserRepository;
+import org.serratec.backend.redesocial.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import jakarta.validation.Valid;
 
@@ -25,30 +28,33 @@ import jakarta.validation.Valid;
 public class UserController {
 
 	@Autowired
-	private UserRepository userRepository;
+	UserService userService; 
+	
 
 	@GetMapping
 	public ResponseEntity<List<User>> listar() {
-		return ResponseEntity.ok(userRepository.findAll());
+		return ResponseEntity.ok(userService.findAll());
 	}
 
 	@GetMapping("/{id}")
 	public ResponseEntity<User> buscar(@PathVariable Long id) {
-		Optional<User> userOpt = userRepository.findById(id);
-		if (userOpt.isPresent()) {
-			return ResponseEntity.ok(userOpt.get());
-		}
-		return ResponseEntity.notFound().build();
+		return ResponseEntity.ok(userService.findById(id));
+		
 	}
 
 	@PostMapping
-	@ResponseStatus(HttpStatus.CREATED)
-	public User inserir(@Valid @RequestBody User user) {
-		user = userRepository.save(user);
-		return user;
+	public ResponseEntity<User> inserir(@RequestBody User user){
+		user = userService.inserir(user);
+		
+		URI uri = ServletUriComponentsBuilder
+				.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(user.getId())
+				.toUri();
+			return ResponseEntity.created(uri).body(user);
 	}
 
-	@PutMapping("/{id}")
+	/*@PutMapping("/{id}")
 	public ResponseEntity<User> atualizar(@PathVariable Long id, @Valid @RequestBody User user) {
 		if (!userRepository.existsById(id)) {
 			return ResponseEntity.notFound().build();
@@ -65,6 +71,6 @@ public class UserController {
 		}
 		userRepository.deleteById(id);
 		return ResponseEntity.noContent().build();
-	}
+	}*/
 
 }
--- src/main/java/org/serratec/backend/redesocial/service/UserService.java
+++ src/main/java/org/serratec/backend/redesocial/service/UserService.java
@@ -1,5 +1,39 @@
 package org.serratec.backend.redesocial.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.serratec.backend.redesocial.exception.EmailException;
+import org.serratec.backend.redesocial.exception.NotFoundException;
+import org.serratec.backend.redesocial.model.User;
+import org.serratec.backend.redesocial.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	public List<User> findAll() {
+		return userRepository.findAll();
+	}
+
+	public User findById(Long id) throws NotFoundException {
+		Optional<User> userOpt = userRepository.findById(id);
+		if (userOpt.isEmpty()) {
+			throw new NotFoundException();
+		}
+		return userOpt.get();
+	}
+
+	public User inserir(User u) throws EmailException {
+		User user = userRepository.findByEmail(u.getEmail());
+		if (user != null) {
+			throw new EmailException("Email já existente");
+		}
+		return userRepository.save(u);
+	}
 
 }
