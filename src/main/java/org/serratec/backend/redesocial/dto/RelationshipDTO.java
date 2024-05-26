fa1506c2b620a2dc9172fa1aefdcafa8274a9bf7
Rafaela Beber Coelho <162373363+rafxbc@users.noreply.github.com>
2024-05-26 00:09:58 -0300

    fix: mudança no atributo 'Date' em relationshipDTO

--- src/main/java/org/serratec/backend/redesocial/dto/RelationshipDTO.java
+++ src/main/java/org/serratec/backend/redesocial/dto/RelationshipDTO.java
@@ -1,21 +1,30 @@
 package org.serratec.backend.redesocial.dto;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
+
+import org.serratec.backend.redesocial.model.Relationship;
 
 public class RelationshipDTO {
 	private Long followerId; // ID do usuário que está seguindo outro usuário.
 	private Long followedId; // ID do usuário que está sendo seguido.
-	private LocalDateTime followedAt; // data e hora em que o usuário começou a seguir o outro usuário.
+	private LocalDate followedAt; // data e hora em que o usuário começou a seguir o outro usuário.
 
 	public RelationshipDTO() {
 		super();
 	}
 
-	public RelationshipDTO(Long followerId, Long followedId, LocalDateTime followedAt) {
+	public RelationshipDTO(Long followerId, Long followedId, LocalDate followedAt) {
 		super();
 		this.followerId = followerId;
 		this.followedId = followedId;
 		this.followedAt = followedAt;
+	}
+	
+	public RelationshipDTO(Relationship relationship) {
+		super();
+		this.followerId = relationship.getRelationshipPK().getFollower().getId();
+		this.followedId = relationship.getRelationshipPK().getFollowed().getId();
+		this.followedAt = relationship.getCreatedAt();
 	}
 
 	public Long getFollowerId() {
@@ -34,11 +43,11 @@ public class RelationshipDTO {
 		this.followedId = followedId;
 	}
 
-	public LocalDateTime getFollowedAt() {
+	public LocalDate getFollowedAt() {
 		return followedAt;
 	}
 
-	public void setFollowedAt(LocalDateTime followedAt) {
+	public void setFollowedAt(LocalDate followedAt) {
 		this.followedAt = followedAt;
 	}
 
