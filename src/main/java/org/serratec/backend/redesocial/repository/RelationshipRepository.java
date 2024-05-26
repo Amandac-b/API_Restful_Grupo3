6a4e2219116054f6ec36b5a31b450d1dccf59f35
Rafaela Beber Coelho <162373363+rafxbc@users.noreply.github.com>
2024-05-26 00:11:18 -0300

    feat: querys em relationship repository

--- src/main/java/org/serratec/backend/redesocial/repository/RelationshipRepository.java
+++ src/main/java/org/serratec/backend/redesocial/repository/RelationshipRepository.java
@@ -1,9 +1,30 @@
 package org.serratec.backend.redesocial.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.serratec.backend.redesocial.model.Relationship;
 import org.serratec.backend.redesocial.model.RelationshipPK;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RelationshipRepository extends JpaRepository<Relationship, RelationshipPK> {
 
+	
+	@Query(value = "select * from relationship where id_seguido = :id", nativeQuery=true)
+    List<Relationship> findAllFollowersByUserId(Long id);
+
+    @Query(value = "select * from relationship where id_seguidor = :id", nativeQuery=true)
+    List<Relationship> findAllFollowingById(Long id);
+    
+    @Query(value = "select * from relationship where id_seguido = :idSeguido and id_seguidor = :idSeguidor", nativeQuery=true)
+    Optional<Relationship> findRelationshipById(Long idSeguido, Long idSeguidor);
+    
+    @Modifying
+    @Query(value = "delete from relationship where id_seguido = :idSeguido and id_seguidor = :idSeguidor", nativeQuery=true)
+    void findAndDelete (Long idSeguido, Long idSeguidor);
+    
 }
