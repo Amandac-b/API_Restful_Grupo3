9909c1a04eb1f5f6ac26161855d99643d27fa67e
Rafaela Beber Coelho <162373363+rafxbc@users.noreply.github.com>
2024-05-26 00:09:31 -0300

    feat: criação dos métodos de relacionamento de User

--- src/main/java/org/serratec/backend/redesocial/controller/UserController.java
+++ src/main/java/org/serratec/backend/redesocial/controller/UserController.java
@@ -3,6 +3,7 @@ package org.serratec.backend.redesocial.controller;
 import java.net.URI;
 import java.util.List;
 
+import org.serratec.backend.redesocial.dto.RelationshipDTO;
 import org.serratec.backend.redesocial.dto.UserDTO;
 import org.serratec.backend.redesocial.dto.UserInserirDTO;
 import org.serratec.backend.redesocial.model.User;
@@ -55,5 +56,38 @@ public class UserController {
 	public ResponseEntity<UserDTO> alterar(@PathVariable Long id, @RequestBody UserInserirDTO novaInfoUser) {
 		return ResponseEntity.ok(userService.save(id, novaInfoUser));
 	}
+	
+	//MÉTODOS DE RELACIONAMENTO
+	
+	@PostMapping("/{idSeguidor}/relationship/follow/{idSeguido}")
+	public ResponseEntity<RelationshipDTO> inserir(@PathVariable Long idSeguido, @PathVariable Long idSeguidor){
+		RelationshipDTO relationshipDTO = userService.seguir(idSeguido, idSeguidor);
+		URI uri = ServletUriComponentsBuilder
+				.fromCurrentRequest().path("/{idSeguidor}/relationship/follow/{idSeguido}")
+				.buildAndExpand(idSeguidor, idSeguido)
+				.toUri();
+		return ResponseEntity.created(uri).body(relationshipDTO);
+	}
+	
+	@DeleteMapping("/{idSeguidor}/relationship/unfollow/{idSeguido}")
+	public ResponseEntity<Void> findAndDelete(@PathVariable Long idSeguidor, @PathVariable Long idSeguido) {
+		try {
+			userService.findAndDelete(idSeguidor, idSeguido);
+			return ResponseEntity.noContent().build();
+		} catch (RuntimeException e) {
+			return ResponseEntity.notFound().build();
+		}
+	}
+	
+	@GetMapping("/{id}/relationship/followers")
+	public ResponseEntity<List<RelationshipDTO>> listarTodosSeguidores(@PathVariable Long id){
+		return ResponseEntity.ok(userService.findAllFollowersByUserId(id));
+	}
 
+	@GetMapping("/{id}/relationship/followed")
+	public ResponseEntity<List<RelationshipDTO>> listarTodosSeguindo(@PathVariable Long id){
+		return ResponseEntity.ok(userService.findAllFollowingById(id));
+	}
+	
+	
 }
